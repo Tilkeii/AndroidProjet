e62bc0a4f5b779453c01d6b45c3e3b01f5e8004b
Thomas Gianfalla <44254960+Her0iik@users.noreply.github.com>
2019-02-26 22:46:35 +0100

    Profil

--- app/src/main/java/com/example/flo/projetandroid/CreateEventActivity.java
+++ app/src/main/java/com/example/flo/projetandroid/CreateEventActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+
 public class CreateEventActivity extends AppCompatActivity {
 
     Toolbar mToolbar;
